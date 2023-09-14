5cd216f5d325a78dcb6b15f1a2a73cd3c19c91e3
Shariar Islam <shariarn85@gmail.com>
2023-09-14 22:35:28 +0600

    did all the course information dynamic and showed it to UI

--- src/Components/Cart/Cart.jsx
+++ src/Components/Cart/Cart.jsx
@@ -2,12 +2,10 @@ import React from 'react';
 
 const Cart = () => {
     return (
-        <div>
+        <div className='mx-auto px-5 mt-5'>
             <h3>Credit Hour Remaining 7 hr</h3>
             <h2>Course Name</h2>
-            <h3>1 Introduction to c programming
-2 Introduction to C++ for DSA
-                3 Software Engineering</h3>
+            <h3>1 Introduction to c programming</h3>
             <h4>Total Credit Hour : 13</h4>
             <h4>Total Price : 48000 USD</h4>
 
--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,5 +1,8 @@
 import React, { useEffect, useState } from 'react';
 import Cart from '../Cart/Cart';
+// import { BsBookHalf } from "react-icons/all-files/fa/FaBeer";
+
+
 
 const Home = () => {
     const [courses, setCourses] = useState([]);
@@ -10,20 +13,37 @@ const Home = () => {
         .then (data=> setCourses(data))
     }, [])
 
-    
+    console.log(courses);
 
     return (
         <div className=' text-center font-bold'>
             <h2 className=' text-center font-bold bg-red-400'>Course Registration of PH with Shariar Islam</h2>
             <div className='flex justify-between'>
                 <div>
-                    <h2>curse here</h2>
+                    <h2>Course here</h2>
+                <div className='grid grid-cols-3'>
                 {
-                // courses.map(course=> )
+                    courses.map((course, idx) => (
+                        <div key={idx} className='mr-3 ml-3 mt-4 mb-5'>
+                                <img className='w-full' src={ course.image}></img>
+                                <h2>{course.courseName}</h2>
+                                <p>{course.shortDescription}</p>
+                                <div className='flex justify-evenly'>
+                                <h4>$ Price: {course.price}</h4>
+                                <h2></h2>
+                                <h4> Credit: ${ course.creditHours}</h4>
+                                </div>
+                                <button className='bg-blue-700 text-white px-5 py-3'>Select</button>
+
+                        </div>
+                ))
                 }
                 </div>
+                </div>
 
-                <Cart></Cart>
+               
+                    <Cart></Cart>
+                
             </div>
         </div>
     );
